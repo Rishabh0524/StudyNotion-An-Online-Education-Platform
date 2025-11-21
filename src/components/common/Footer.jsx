f19a5ce397307e4fb440d7767c6d51b959f89dc3
Rishabh Malviya <rishabh212malviya@gmail.com>
2025-11-21 20:57:23 +0530

    Added vercel.json config

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -172,7 +172,9 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">
+            Made with ❤️ Rishabh MAlviya © 2025 Studynotion
+          </div>
         </div>
       </div>
     </div>
